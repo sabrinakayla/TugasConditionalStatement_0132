0d0fa87f770265091080a5b1173d0877843aa81d
sabrinakayla <sabrinakuliah10@gmail.com>
2026-03-10 19:29:01 +0700

    menambahkan prosedur

--- tugas.cpp
+++ tugas.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+void masukanData(float &berat, float &tinggi) {
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
