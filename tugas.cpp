e52d9428c548efbc421bd69dad94956f058679b7
sabrinakayla <sabrinakuliah10@gmail.com>
2026-03-10 19:30:19 +0700

    menambahkan fungsi hitung nilai BMI

--- tugas.cpp
+++ tugas.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 void masukanData(float &berat, float &tinggi) {
-    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+    cout << "--- Selamat Datang Mahasiswa Kelas C 2025 ---" << endl;
     cout << "Masukkan berat badan (kg): ";
     cin >> berat;
     cout << "Masukkan tinggi badan (meter): ";
     cin >> tinggi;
+}
+
+float hitungBMI(float berat, float tinggi) {
+    return berat / (tinggi * tinggi);
 }
